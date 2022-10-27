cc3fb24a10f5cbd63b77244660b1cda79aaf482c
carlos.quadros.br <carlos.silva@sunset.com.py>
2022-10-27 10:46:37 -0300

    first class

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor > saldo)
+                return false;       
+            saldo -= valor;
+            return true;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+                return false;
+            saldo -= valor;
+            destino.saldo += valor;
+            return true;
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,19 @@
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-x";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo do "+ contaDoAndre.titular + "  "+ contaDoAndre.saldo);
+
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDoAndre.titular = "Maria Souza";
+contaDoAndre.numero_agencia = 17;
+contaDoAndre.conta = "1010-5";
+contaDoAndre.saldo = 350;
+
+Console.WriteLine("Saldo do " + contaDaMaria.titular + "  " + contaDaMaria.saldo);
+
+Console.WriteLine("Saldo do André " + contaDoAndre.saldo);
